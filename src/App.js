1bda4e29f4db933cbaf0441ffd75bafa3754757e
Fariz Rachmansyah <farizrachmansyah7@gmail.com>
2022-09-04 10:27:36 +0700

    localstorage integration

--- src/App.js
+++ src/App.js
@@ -3,30 +3,25 @@ import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
-  const DUMMY_EXPENSES = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14)
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28)
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12)
-    }
-  ];
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  // check for the expenses storage
+  let EXPENSES_STORAGE;
+  if (localStorage.getItem('EXPENSES_STORAGE') === null) {
+    EXPENSES_STORAGE = [];
+  } else {
+    EXPENSES_STORAGE = JSON.parse(localStorage.getItem('EXPENSES_STORAGE'));
+    EXPENSES_STORAGE.forEach((item) => {
+      item.date = new Date(item.date);
+    });
+  }
+
+  const [expenses, setExpenses] = useState(EXPENSES_STORAGE);
   const addExpenseHandler = (expense) => {
-    setExpenses((prevState) => [...prevState, expense]);
+    setExpenses((prevState) => {
+      const data = [...prevState, expense];
+      console.log(data);
+      localStorage.setItem('EXPENSES_STORAGE', JSON.stringify(data));
+      return data;
+    });
   };
 
   return (
